1d29993644ce4f1ddcaba58b531825dbe78c3233
aadil42 <adilkelawala@gmail.com>
2022-12-14 03:43:31 +0530

    solved the error by removeing the node_module and runnig the package.json from the tutorial to make sure the version of modules were correct and It ran.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import "App.css";
+
+const App = () => (
+    <div>
+        <h1>hello React this is set up custom way</h1>
+    </div>
+);
+
+
+export default App;
