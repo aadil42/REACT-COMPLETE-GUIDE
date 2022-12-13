ebe3ea56a31cd02ff29c31cf876357144f565073
aadil42 <adilkelawala@gmail.com>
2022-12-14 03:59:07 +0530

    importing random image into the setup app

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import ReactDom from 'react-dom';
 
 import "App.css";
 
+import tiger from "./mike-marrah-gRB4Euk4BYQ-unsplash.jpg";
+
 const App = () => (
     <div>
         <h1>hello React this is set up custom way</h1>
+        <img src={tiger} />
     </div>
 );
 
